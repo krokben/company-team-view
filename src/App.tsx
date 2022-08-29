5b3dec3a4f6f7f4b48d80545c91574441b9c641a
Tommy Svensson <4046409+krokben@users.noreply.github.com>
2022-08-29 19:20:05 +0200

    fetch members

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+type Member = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  email: string;
+  picture: string;
+  city: string;
+};
+
+type ApiMember = {
+  login: { uuid: string };
+  name: {
+    first: string;
+    last: string;
+  };
+  phone: string;
+  email: string;
+  picture: { thumbnail: string };
+  location: { city: string };
+};
+
+enum Status {
+  Idle,
+  Fetching,
+  Success,
+  Error,
+}
 
 const App = () => {
-  return <div>Hello World!</div>;
+  const [members, setMembers] = useState<Member[]>([]);
+  const [status, setStatus] = useState<Status>(Status.Idle);
+
+  useEffect(() => {
+    setStatus(Status.Fetching);
+
+    fetch("https://randomuser.me/api/?results=50")
+      .then((response) => response.json())
+      .then(({ results }: { results: ApiMember[] }) => {
+        setMembers(
+          results.map(({ login, name, phone, email, picture, location }) => ({
+            id: login.uuid,
+            firstName: name.first,
+            lastName: name.last,
+            phone,
+            email,
+            picture: picture.thumbnail,
+            city: location.city,
+          }))
+        );
+        setStatus(Status.Success);
+      })
+      .catch((error) => {
+        console.error(error);
+        setStatus(Status.Error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {
+        <ul>
+          {status === Status.Success ? (
+            members.map(({ id, firstName, lastName }) => (
+              <li key={id}>{`${firstName} ${lastName}`}</li>
+            ))
+          ) : (
+            <li>idle/fetching/error</li>
+          )}
+        </ul>
+      }
+    </div>
+  );
 };
 
 export default App;
