0296026c49d25fb0ecb4a8491b9065f9e1a3f731
Tommy Svensson <4046409+krokben@users.noreply.github.com>
2022-08-30 07:31:48 +0200

    add sorting

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,7 @@ const App = ({ url }: { url: string }) => {
   const [status, setStatus] = useState<Status>(Status.Idle);
   const [members, setMembers] = useState<Member[]>([]);
   const [isGrid, setIsGrid] = useState<boolean>(false);
+  const [sortDescending, setSortDescending] = useState<boolean>(false);
 
   useEffect(() => {
     setStatus(Status.Fetching);
@@ -42,15 +43,19 @@ const App = ({ url }: { url: string }) => {
       .then((response) => response.json())
       .then(({ results }: { results: ApiMember[] }) => {
         setMembers(
-          results.map(({ login, name, phone, email, picture, location }) => ({
-            id: login.uuid,
-            firstName: name.first,
-            lastName: name.last,
-            phone,
-            email,
-            picture: picture.large,
-            city: location.city,
-          }))
+          results
+            .map(({ login, name, phone, email, picture, location }) => ({
+              id: login.uuid,
+              firstName: name.first,
+              lastName: name.last,
+              phone,
+              email,
+              picture: picture.large,
+              city: location.city,
+            }))
+            .sort((a: Member, b: Member) =>
+              a.lastName.localeCompare(b.lastName)
+            )
         );
         setStatus(Status.Success);
       })
@@ -64,10 +69,21 @@ const App = ({ url }: { url: string }) => {
     <main>
       <h1>Meet the Team</h1>
       <nav>
+        <button
+          className="sort-toggle"
+          onClick={() => setSortDescending(!sortDescending)}
+        />
         <button className="grid-toggle" onClick={() => setIsGrid(!isGrid)} />
       </nav>
       {status === Status.Success ? (
-        <Members members={members} isGrid={isGrid} />
+        <Members
+          members={members.sort((a: Member, b: Member) =>
+            sortDescending
+              ? b.lastName.localeCompare(a.lastName)
+              : a.lastName.localeCompare(b.lastName)
+          )}
+          isGrid={isGrid}
+        />
       ) : (
         <ul>
           <li>idle/fetching/error</li>
